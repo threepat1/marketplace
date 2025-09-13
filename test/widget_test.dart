7c29d8d01358a7c3998ea8aa92b63b5f22069123
threepat1 <42788704+threepat1@users.noreply.github.com>
2025-09-14 00:08:57 +0700

    try to create unit test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,17 +5,30 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:marketplace/data/models/product_model.dart';
 import 'package:marketplace/main.dart';
 import 'package:marketplace/presentation/home/main_screen.dart'; // adjust import if main.dart is in a different folder
 
 void main() {
-  testWidgets('App builds main screen', (WidgetTester tester) async {
-    // Pump the app
-    await tester.pumpWidget(const MyApp());
+  Map<String, dynamic> readFixture() {
+    final file = File('test/fixtures/product.json');
+    final jsonString = file.readAsStringSync();
+    return jsonDecode(jsonString) as Map<String, dynamic>;
+  }
 
-    // Verify MainScreen is in the widget tree
-    expect(find.byType(MainScreen), findsOneWidget);
+  group('ProductModel.fromJson', () {
+    test('parses essential fields', () {
+      final jsonMap = readFixture();
+      final product = ProductModel.fromJson(jsonMap);
+
+      expect(product.name, 'john_doe');
+      expect(product.currentBid, '115');
+      expect(product.endTime, "2024-05-17T12:00:00.000Z");
+    });
   });
 }
